cf4a7bc1f7613d65bf44bfdbe1a0138986d0dcb1
zmurku <zuzannamurek@gmail.com>
2020-03-11 22:14:34 +0100

    grid

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 
+let width  = 600;
+let height = 600;
 let renderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.setSize(width,height)
 document.body.appendChild(renderer.domElement)
 
 let fieldOfViewDegrees = 45 
@@ -36,7 +38,7 @@ varying vec2 currentPixelPosition;
 void main() {
 	currentPixelPosition.x = position.x;
 	currentPixelPosition.y = position.y;
-	currentPixelPosition = currentPixelPosition * 10.0;
+	currentPixelPosition = currentPixelPosition * 600.0;
 	// Contains the position of the current vertex.
     gl_Position = vec4(position.x,position.y,position.z,1.0); 
 }
@@ -44,6 +46,11 @@ void main() {
 `
 
 let fragmentShader = `
+
+uniform vec2 u_resolution;
+uniform float u_time;
+
+
 // Position of te pixel that we are currently drawing in the space [-10,10] x [-10,10].
 varying vec2 currentPixelPosition;
 
@@ -68,6 +75,16 @@ float distanceToRectangle(vec2 samplePosition, vec2 halfSize){
     return outsideDistance + insideDistance;
 }
 
+float distanceToLineVertical(vec2 samplePosition, vec2 halfSize){
+    vec2 componentWiseEdgeDistance = abs(samplePosition) - halfSize;
+    return componentWiseEdgeDistance.x;
+}
+
+float distanceToLineHorizontal(vec2 samplePosition, vec2 halfSize){
+    vec2 componentWiseEdgeDistance = abs(samplePosition) - halfSize;
+    return componentWiseEdgeDistance.y;
+}
+
 /// Returns distance to a combined shape.
 float merge(float distanceToShape1, float distanceToShape2){
     return min(distanceToShape1, distanceToShape2);
@@ -89,6 +106,18 @@ float grow(float distance, float size){
     return distance - size;
 }
 
+vec2 repeatX(vec2 position, float distance) {
+	float x = mod(position.x, distance);
+	float y = position.y;
+	return vec2(x, y);
+}
+
+vec2 repeatY(vec2 position, float distance) {
+	float y = mod(position.y, distance);
+	float x = position.x;
+	return vec2(x, y);
+}
+
 vec2 translate(vec2 samplePosition, vec2 offset){
 	return samplePosition - offset;
 }
@@ -107,23 +136,51 @@ vec2 scale(vec2 samplePosition, float scale){
 }
 
 
-float scene(vec2 position) {
-	vec2 position2 = translate(position, vec2(3.0, 0.0));
-	float rectangle = distanceToRectangle(position2, vec2(2.0, 2.0));
-	float circle = distanceToCircle(position, 2.0);
-	float sub = subtract(rectangle, circle);
-	float growth = grow(sub, 0.1);
-	float sub2 = subtract(growth, sub);
-	return sub2;
+float grid_distance(vec2 position) {
+	vec2 position2 = translate(position, vec2(2.0, 0.0));
+	vec2 repPositionX = repeatX(position, 40.0);
+	vec2 repPositionY = repeatY(position, 40.0);
+	float lineVertical   = distanceToLineVertical(repPositionX, vec2(2.0, 0.0));
+	float lineHorizontal = distanceToLineHorizontal(repPositionY, vec2(0.0, 2.0));
+	float add = merge(lineVertical, lineHorizontal);
+	return add;
+}
+
+float render (float distance) {
+	float distanceChange = fwidth(distance) * 0.5;
+	float antialiasedCutoff = smoothstep(distanceChange, -distanceChange, distance);
+	return antialiasedCutoff;
+}
+
+vec4 grid(vec2 position) {
+	float distance = grid_distance(position);
+	float alpha    = render(distance);
+	return vec4(1.0, 1.0, 1.0, alpha*0.03);
+}
+
+float background(vec2 position) {
+	float rectangle = distanceToRectangle(position, vec2(600.0, 600.0));
+	return rectangle;
+}
+
+vec4 mix_colors(vec4 bg, vec4 fg) {
+	vec4 p_fg = fg * fg.a;
+	vec4 p_bg = bg * bg.a;
+	vec4 p_cd = (1.0 - fg.a) * p_bg + p_fg;
+	vec4 cd   = p_cd / p_cd.a;
+	return cd;
 }
 
 // Execute for every pixel.
 void main() {
-	float dist = scene(currentPixelPosition);
-	float distanceChange = fwidth(dist) * 0.5;
-	float antialiasedCutoff = smoothstep(distanceChange, -distanceChange, dist);
-	gl_FragColor = vec4(antialiasedCutoff,antialiasedCutoff,antialiasedCutoff,1.0);
+	vec4 grid_color = grid(currentPixelPosition);
+	vec4 bg_color   = vec4(0.03, 0.15, 0.26 ,1.0);
+	vec4 out_color  = mix_colors(bg_color,grid_color);
+
+	gl_FragColor = out_color;
+
 }
+
 
 `
 
